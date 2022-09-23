f362213800db7f583cf4431215770f19c6e67615
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-09-24 02:57:04 +0600

    index-php design

--- index.php
+++ index.php
@@ -31,15 +31,24 @@
             <div class="col-md-4"></div>
         </div>
     </header>
-    <main>
-        <div>
-            <div id="admin">
-                <button id="admin_button" >admin</button>
-            </div>
-            <div id="students">
-               <button id="students_button">students</button>
-            </div>
+    <main style="width:100%;">
+        <div class="row">
+    <div class="col-6">
+        <div class="card" id="admin_button">>
+        <div class="card-body bg-primary rounded">
+            <h5 class="card-title p-5  fs-1 text-center fw-semi-bold">ADMIN</h5>
         </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card" id="students_button">>
+        <div class="card-body bg-warning rounded">
+            <h5 class="card-title p-5  fs-1 text-center fw-semi-bold" id="">STUDENT</h5>
+            
+        </div>
+        </div>
+    </div>
+    </div>
     </main>
     <!-- bootstrap file connect (js) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
